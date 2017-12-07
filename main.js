0d1d1a9d1308105765c5fac63ed1663f908ad3a8
rover33 <remy.j.pearlstone@gmail.com>
2017-12-06 17:59:27 -0700

    tic tac toe

---
+++ main.js
@@ -0,0 +1,19 @@
+var counter = 0;
+
+$("td").click(function() {
+    if ($(this).html() == '') {
+        if ((counter % 2) == 0) {
+            $(this).html('x');
+            counter ++;
+        } else {
+            $(this).html('o');
+            counter ++;
+        }  
+    } 
+    
+    
+    
+
+});
+
+
